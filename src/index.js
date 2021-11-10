3526b2b3633b92b76293c6c4c29377bbe13d4e75
VictorBorzaquel <victorborzaquel@outlook.com>
2021-11-10 15:15:58 -0300

    complete

--- src/index.js
+++ src/index.js
@@ -51,7 +51,7 @@ app.post('/users', (request, response) => {
 
 	const user = {
 		id: uuid.v4(),
-		name, 
+		name,
 		username,
 		todos: [],
 	};
@@ -96,7 +96,7 @@ app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
 	}
 
 	if (!!title) todo.title = title;
-	if (!!deadline) todo.deadline = deadline;
+	if (!!deadline) todo.deadline = new Date(deadline);
 
 	return response.status(200).json(todo);
 });
@@ -120,13 +120,13 @@ app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
 	const { user } = request;
 	const { id } = request.params;
 
-	const todo = user.todos.find(todo => todo.id === id);
+	const todoIndex = user.todos.findIndex(todo => todo.id === id);
 
-	if (!todo) {
+	if (todoIndex === -1) {
 		return response.status(404).json({ error: 'Todo not found' });
 	}
 
-	user.todos.splice(todo, 1);
+	user.todos.splice(todoIndex, 1);
 
 	return response.status(204).send();
 });
